4ff7fe75edcd86fe3e840dfd5b58e764782e75f1
sylviahe171 <sylviahe171@gmail.com>
2024-07-25 16:26:07 +0800

    multiple page to pdf with no issue in the shape of input photo is done

--- example/lib/image_pdf_api.dart
+++ example/lib/image_pdf_api.dart
@@ -15,14 +15,28 @@ class ImagePdfApi {
     for (var path in _paths) {
       Uint8List image1 = await File(path).readAsBytes();
 
-      pdf.addPage(
-        MultiPage(
-          pageTheme: pageTheme,
-          build: (context) => [
-            Image(MemoryImage(image1)),
-          ],
-        ),
-      );
+      //dumb way to avoid width or height of the image out of bound
+      try {
+        pdf.addPage(
+          MultiPage(
+            pageTheme: pageTheme,
+            build: (context) => [
+              Image(MemoryImage(image1),
+                  width: pageTheme.pageFormat.availableWidth - 1)
+            ],
+          ),
+        );
+      } catch (e) {
+        pdf.addPage(
+          MultiPage(
+            pageTheme: pageTheme,
+            build: (context) => [
+              Image(MemoryImage(image1),
+                  height: pageTheme.pageFormat.availableHeight - 1)
+            ],
+          ),
+        );
+      }
     }
 
     return SaveAndOpenDocument.savePdf(name: 'image_pdf.pdf', pdf: pdf);
