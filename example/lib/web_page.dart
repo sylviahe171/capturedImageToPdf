f9ff24179c47bb690336e1a06772cacd2dbf1da0
sylviahe171 <sylviahe171@gmail.com>
2024-07-29 12:00:01 +0800

    flutter webview text field autofill done

--- example/lib/web_page.dart
+++ example/lib/web_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+import 'package:webview_flutter_platform_interface/webview_flutter_platform_interface.dart';
 
 class WebViewContainer extends StatefulWidget {
   @override
@@ -7,15 +9,26 @@ class WebViewContainer extends StatefulWidget {
 }
 
 class _WebViewContainerState extends State<WebViewContainer> {
-  late final controller = WebViewController()
-    ..setJavaScriptMode(JavaScriptMode.disabled)
-    ..loadRequest(Uri.parse('https://tally.so/r/3qP8lO'));
+  late final controller = PlatformWebViewController(
+    AndroidWebViewControllerCreationParams(),
+  )
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..loadRequest(
+        LoadRequestParams(uri: Uri.parse('https://tally.so/r/3qP8lO')));
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("webview container"),
         ),
-        body: WebViewWidget(controller: controller));
+        body: PlatformWebViewWidget(
+          PlatformWebViewWidgetCreationParams(controller: controller),
+        ).build(context),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.import_export),
+            onPressed: () async {
+              controller.runJavaScript(
+                  'document.getElementById("dd2bd7d8-4274-42b8-b7b7-6a3bc527ca47").value = "New Value";');
+            }));
   }
 }
